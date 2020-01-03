e3b01fedc86e24105bf855d4c347d0769ec91f6e
cyrus <cyruscyliu@gmail.com>
2020-01-03 12:33:08 +0800

    fuck firmadyne hard to type

--- quick.sh
+++ quick.sh
@@ -1,2 +1,2 @@
-./evalution.py -dbt firmadyne -wd ../salamander-build -q > firmadyne.sh
+./evalution.py -dbt firmadyne -wd ../salamander-build -q > all.sh
 # ./evalution.py -dbt text -wd ../salamander-build -q > text.sh

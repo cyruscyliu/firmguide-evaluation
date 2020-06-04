8df5a363826d3fdf0b9e476ad1a00af5ca2fc3b1
Qiang <liuqiang@zju.edu.cn>
2020-06-04 08:54:33 +0000

    update ns16550 results

--- ns16550a-statistics.py
+++ ns16550a-statistics.py
@@ -10,24 +10,25 @@ def hardware(args):
     i = 1
 
     c_all = {}
-    for dtb in os.listdir('dtb'):
-        print('{:04}'.format(i), dtb)
+    for root, dirs, files in os.walk('dtb'):
+        for dtb in files:
+            print('{:04}'.format(i), dtb)
+            dts = load_dtb(os.path.join(root, dtb))
 
-        dts = load_dtb(os.path.join('dtb', dtb))
-
-        try:
-            serials = find_flatten_serial_in_fdt(dts)
-            if serials is None:
-                continue
-            for serial in serials:
-                compatible = serial['compatible'][-1]
-                if compatible in c_all:
-                    c_all[compatible] += 1
-                else:
-                    c_all[compatible] = 0
-        except Exception as e:
-            print('>>>> ERROR <<<< {}'.format(e))
-        i += 1
+            try:
+                serials = find_flatten_serial_in_fdt(dts)
+                if serials is None:
+                    i += 1
+                    continue
+                for serial in serials:
+                    compatible = serial['compatible'][-1]
+                    if compatible in c_all:
+                        c_all[compatible] += 1
+                    else:
+                        c_all[compatible] = 0
+            except Exception as e:
+                print('>>>> ERROR <<<< {}'.format(e))
+            i += 1
 
     with open('ns16550a-statistics.txt', 'w') as f:
         for k, v in c_all.items():
